7fd5e5f761ca61dd70849363f05a7b282174fc1f
marci <jackjack2000.kahp@gmail.com>
2024-08-21 08:54:25 +0700

    init code

---
+++ ui/lib/src/index.tsx
@@ -0,0 +1,7 @@
+import "./scss/stylesheet.scss";
+import * as server from "./server";
+import * as input from "./input";
+import * as widget from "./widget";
+import * as hook from "./custom-hook";
+
+export { server, input, widget, hook };
---
+++ ui/lib/src/input/index.tsx
@@ -0,0 +1,5 @@
+export * from "./FieldString";
+export * from "./FieldNumber";
+export * from "./FieldText";
+export * from "./FieldDate";
+export * from "./FieldCheckBox";
