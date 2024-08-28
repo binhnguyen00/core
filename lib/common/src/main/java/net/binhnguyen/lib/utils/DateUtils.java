c99f7bd2a43cca7560d12a95d807a04b4744140a
marci <jackjack2000.kahp@gmail.com>
2024-08-28 16:13:03 +0700

    enhance DateUtils

--- lib/common/src/main/java/net/binhnguyen/lib/utils/DateUtils.java
+++ lib/common/src/main/java/net/binhnguyen/lib/utils/DateUtils.java
@@ -1,5 +1,6 @@
 package net.binhnguyen.lib.utils;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
@@ -10,17 +11,29 @@ public class DateUtils {
   public static final String timestamp    = "dd-MM-yyyy HH:mm:ss";
   public static final String timestampTz  = "dd-MM-yyyy HH:mm:ssZ";
 
-  public String reformatToTimestamp(LocalDateTime target) {
+  public static LocalDate toLocalDate(String target) {
+    return LocalDate.parse(target);
+  }
+
+  public static LocalDateTime toLocalDateTime(String target) {
+    return LocalDateTime.parse(target);
+  }
+
+  public static ZonedDateTime toZonedDateTime(String target) {
+    return ZonedDateTime.parse(target);
+  }
+
+  public static String toTimestampString(LocalDateTime target) {
     DateTimeFormatter formatter = DateTimeFormatter.ofPattern(timestamp);
     return target.format(formatter);
   }
 
-  public String reformatToTimestampTz(ZonedDateTime target) {
+  public static String toTimestampTzString(ZonedDateTime target) {
     DateTimeFormatter formatter = DateTimeFormatter.ofPattern(timestampTz);
     return target.format(formatter);
   }
 
-  public String reformatToDate(LocalDateTime target) {
+  public static String toLocalDateString(LocalDateTime target) {
     DateTimeFormatter formatter = DateTimeFormatter.ofPattern(date);
     return target.format(formatter);
   }
